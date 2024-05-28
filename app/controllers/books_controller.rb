5e45461b79df1116bb9e9c0e8c4a9e730fbb5c79
yuki-9651 <165871172+yuki-9651@users.noreply.github.com>
2024-05-28 08:56:56 +0000

    [Add]ﾌﾗｯｼｭﾒｯｾｰｼﾞ･ﾊﾞﾘﾃﾞｰｼｮﾝの実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,14 @@
 class BooksController < ApplicationController
 
-  
   def create
-   
     @book = Book.new(book_params)
-   
-    @book.save
- 
-    redirect_to books_path(@book.id)
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to show_book_path(@book.id)
+    else
+      @books = Book.all 
+      render :index
+    end
   end
 
   def index
@@ -24,9 +25,14 @@ class BooksController < ApplicationController
   end
   
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to books_path 
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+      redirect_to show_book_path(@book.id)
+    else
+      @books = Book.all 
+      render :edit
+    end
   end
   
   def destroy
@@ -34,10 +40,10 @@ class BooksController < ApplicationController
     book.destroy
     redirect_to '/books'
   end
-  
+
   private
-  
+
   def book_params
-   params.require(:book).permit(:title, :body)
+    params.require(:book).permit(:title, :body)
   end
 end
