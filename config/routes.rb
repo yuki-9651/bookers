70a63e0b11104c7a053e701f4529f021822122b5
yuki-9651 <165871172+yuki-9651@users.noreply.github.com>
2024-05-29 09:06:55 +0000

    [Finish]3-16

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get 'books/:id' => 'books#show', as: 'show_book'
   get 'books/:id/edit' => 'books#edit', as: 'edit_book'
   get '/top' => 'homes#top'
-  
+  root to: 'homes#top'
   post '/books' => 'books#create'
   patch '/books/:id' => 'books#update', as: 'update_book'
   delete 'books/:id' => 'books#destroy', as: 'destroy_book'
--- db/schema.rb
+++ db/schema.rb
@@ -12,13 +12,6 @@
 
 ActiveRecord::Schema.define(version: 2024_05_27_022004) do
 
-  create_table "bookers", force: :cascade do |t|
-    t.string "title"
-    t.string "body"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.string "body"
